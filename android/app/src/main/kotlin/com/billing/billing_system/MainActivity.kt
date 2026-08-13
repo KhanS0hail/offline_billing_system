12d790b24da8977c45f15707eeb17d644c1434ac
KhanS0hail <120774830+KhanS0hail@users.noreply.github.com>
2026-08-13 11:35:43 +0000

    Day 1 Complete: Database Engine, Main Shell, Dark Mode

--- android/app/src/main/kotlin/com/billing/billing_system/MainActivity.kt
+++ android/app/src/main/kotlin/com/billing/billing_system/MainActivity.kt
@@ -2,4 +2,4 @@ package com.billing.billing_system
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_sqlcipher
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqfliteSqlCipherPlugin.register(with: registry.registrar(forPlugin: "SqfliteSqlCipherPlugin"))
 }
