c665992fd5c9668adfe495405f252f64dc2c2020
KhanS0hail <120774830+KhanS0hail@users.noreply.github.com>
2026-08-15 19:45:13 +0530

    Bump Android compileSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.billing.billing_system"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
